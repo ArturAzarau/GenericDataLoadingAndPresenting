3324e9f6bffb307df7b831b751e973ada72e332e
Artur Azarau <artur.azarau@gmail.com>
2020-08-08 23:00:39 +0300

    Initial Project

---
+++ GenericDataLoadingAndPresenting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericDataLoadingAndPresenting
+//
+//  Created by Личный on 08.08.2020.
+//  Copyright © 2020 Artur Azarau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
